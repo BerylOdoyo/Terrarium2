40a6848ce5db5f93caddf802d16df1975c914d22
BerylOdoyo <berylkimberly@gmail.com>
2022-09-27 05:22:56 +0300

    the two functions are in charge of stating what happens when you drag a plant and stop dragging it

--- script.js
+++ script.js
@@ -29,4 +29,20 @@ function dragElement(terrariumElement) {
     document.onpointermove = elementDrag;
     document.onpointerup = stopElementDrag;
   }
+
+  function elementDrag(e) {
+	pos1 = pos3 - e.clientX;
+	pos2 = pos4 - e.clientY;
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	console.log(pos1, pos2, pos3, pos4);
+	terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
+	terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+}
+
+function stopElementDrag() {
+	document.onpointerup = null;
+	document.onpointermove = null;
+}
+
 }
